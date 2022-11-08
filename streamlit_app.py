23720b630c09a8d7bbf6f42b3b150fbd411c7a1e
wfrisbie2 <115744098+wfrisbie2@users.noreply.github.com>
2022-11-08 16:31:45 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#New Section to display fruityvice api response
+#Create the repeatable code blocked (called a function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+#New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice 
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
  except URLERROR as e:
-  streamlit.error()
+    streamlit.error()
   
  streamlit.write('The user entered ', fruit_choice)
 
